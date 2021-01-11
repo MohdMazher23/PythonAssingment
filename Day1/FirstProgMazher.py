d785ba77ec0765761beda781e3ec03a67fdf74f4
Mohd Mazher Uddin <mohdmazheruddin1997@gmail.com>
2021-01-11 14:18:34 +0530

    Add files via upload

---
+++ Day1/FirstProgMazher.py
@@ -0,0 +1,11 @@
+# My name is Mohd Mazher Uddin
+# this is my first python program
+#sension 1
+#assingment
+
+
+print("Welcom to Python Tranning")
+
+#output
+
+#====== RESTART: C:\Users\MAZHER\Desktop\MazherPython\FirstProgMazher.py ======Welcom to Python Tranning
